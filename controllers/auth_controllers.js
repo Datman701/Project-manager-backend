f96110da7c32be49423ae8ab07b58f2ce530e564
Pateel Deepesh Kumar Reddy <deepu.datman@gmail.com>
2025-10-24 15:02:51 +0530

    Change cookie settings to secure and sameSite none
    Updated cookie settings for token to enhance security.

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -39,8 +39,8 @@ export const signUp = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24
     });
 
@@ -83,8 +83,8 @@ export const signIn = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24
     });
 
